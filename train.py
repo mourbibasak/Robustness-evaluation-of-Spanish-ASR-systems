6a5d9ab6c7fe28dc1d417b03ffbd7c487d217ee4
Mourbi <mourbibasak@gmail.com>
2024-08-25 07:37:18 +0100

    meow meow

--- train.py
+++ train.py
@@ -13,22 +13,16 @@ asr_model = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(model_name="stt_es
 TOKENIZER_DIR = "/home/mourb/nemo/train/tokenizer_spe_bpe_v1024"
 asr_model.change_vocabulary(new_tokenizer_dir=TOKENIZER_DIR, new_tokenizer_type="bpe")
      
-
-
-
- 
-
-
+## Checking parameters of pretrained model
 # params = OmegaConf.load("conformer_ctc_bpe.yaml")
 # print(OmegaConf.to_yaml(params))
 
 
 #setup data loaders
-
 cfg = copy.deepcopy(asr_model.cfg)
 # print(cfg.decoding)
 
-#trying with removing SpecAugment
+#removing SpecAugment
 # cfg.spec_augment.freq_masks = 0
 # cfg.spec_augment.time_masks = 0
 
@@ -38,9 +32,10 @@ cfg.tokenizer.dir = TOKENIZER_DIR
 cfg.tokenizer.type = "bpe"
 asr_model.cfg.tokenizer = cfg.tokenizer
 
-# pretrained_decoder = asr_model.decoder.state_dict() 
-#preserving the parameters of the pretrained decoder incase weight matching can be done later.
+''' to preserve the parameters of the pretrained decoder uncomment 
+'''
 
+# pretrained_decoder = asr_model.decoder.state_dict()
 # if asr_model.decoder.decoder_layers[0].weight.shape == pretrained_decoder['decoder_layers.0.weight'].shape:
 #     asr_model.decoder.load_state_dict(pretrained_decoder)
 #     logging.info("Decoder shapes matched - restored weights from pre-trained model")
@@ -55,7 +50,7 @@ asr_model.cfg.tokenizer = cfg.tokenizer
 with open_dict(cfg):
     
     #train dataset
-    cfg.train_ds.manifest_filepath = "/home/mourb/nemo/train/train_manifest.json"
+    cfg.train_ds.manifest_filepath = "./train/train_manifest.json"
     cfg.train_ds.batch_size = 8
     cfg.train_ds.num_workers = 8
     cfg.train_ds.pin_memory = True
@@ -67,7 +62,7 @@ with open_dict(cfg):
 
 
     # validation dataset
-    cfg.validation_ds.manifest_filepath = "/home/mourb/nemo/dev/dev_manifest.json"
+    cfg.validation_ds.manifest_filepath = "./dev/dev_manifest.json"
     cfg.validation_ds.batch_size = 8
     cfg.validation_ds.num_workers = 8
     cfg.validation_ds.pin_memory = False
@@ -120,27 +115,15 @@ trainer = ptl.Trainer(devices=-1,
                       log_every_n_steps=5,
                       check_val_every_n_epoch=5)
 
-# Setup model with the trainer
 
+'''for wandb_logging uncomment below. Using MLFlow for this training'''
+# exp_manager.create_wandb_logger=True
+# exp_manager.wandb_logger_kwargs.name="CONFORMER CTC Spanish"
+# exp_manager.wandb_logger_kwargs.project="Training"
 
-
-#     trainer.devices=-1
-#     trainer.accelerator='gpu'
-#     trainer.strategy='ddp',
-#     trainer.max_epochs=50,
-#     trainer.accumulate_grad_batches=4,
-#     trainer.check_val_every_n_epoch=10.
-#     trainer.log_every_n_steps=5
-
-
-    # exp_manager.create_wandb_logger=True
-    # exp_manager.wandb_logger_kwargs.name="CONFORMER CTC Spanish"
-    # exp_manager.wandb_logger_kwargs.project="Training"
 
 
 #create MLFlow logger 
-
-
 config = exp_manager.ExpManagerConfig(exp_dir="./Conformer_Spanish", name="CONFORMER_large_es_to_spanish", 
         create_checkpoint_callback=True, 
         create_tensorboard_logger=False,
@@ -160,7 +143,7 @@ logdir = exp_manager.exp_manager(trainer, config)
 asr_model.set_trainer(trainer)
 asr_model.cfg = asr_model._cfg
 trainer.fit(asr_model)
-asr_model.save_to("Conformer_es_large_2.nemo")
+asr_model.save_to("Conformer_es_large.nemo")
 
 
 
